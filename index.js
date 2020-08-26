dd60992e6adbaf60a39dd74356c21a57646a17ae
kira56 <ridhunter1508@gmail.com>
2020-08-25 19:55:46 -0500

    port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }))
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
 })
 
 // arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto : ${PORT}`)
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto : ${port}`)
 })
